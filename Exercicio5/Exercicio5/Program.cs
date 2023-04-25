0b13bc09cf22ec6d4999e1c9b4fc429726c6f12e
Thiago Ferreira do Prado <95104754+ThiagoFerreiradoPrado@users.noreply.github.com>
2023-04-25 17:51:15 -0300

    Add files via upload

---
+++ Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,31 @@
+﻿using Cours;
+using System;
+using System.Globalization;
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Retangulo ret = new Retangulo();
+           
+            Console.WriteLine("Entre a largura e a altura do retangulo:");
+
+            ret.Largura = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            ret.Altura = Double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area: "+ ret.Area().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro: "+ret.Perimetro().ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal:"+ ret.Diagonal().ToString("F2",CultureInfo.InvariantCulture));
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
+
